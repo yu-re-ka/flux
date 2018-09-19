1f14c9d47d10bef05fba3787639169737bf8bb84
Jeff Wendling <jeff@influxdata.com>
2018-09-19 16:17:38 -0700

    oops linux has sed not gsed

--- functions/futhark/agg_linux.go
+++ functions/futhark/agg_linux.go
@@ -5,8 +5,8 @@ import "C"
 //go:generate futhark-opencl --library agg.fut -o agg_gpu
 //go:generate futhark-c --library agg.fut -o agg_cpu
 
-//go:generate gsed -i s/futhark/futhark_gpu/g agg_gpu.c agg_gpu.h
-//go:generate gsed -i s/futhark/futhark_cpu/g agg_cpu.c agg_cpu.h
+//go:generate sed -i s/futhark/futhark_gpu/g agg_gpu.c agg_gpu.h
+//go:generate sed -i s/futhark/futhark_cpu/g agg_cpu.c agg_cpu.h
 
 //go:generate gcc -o libagg.so -fPIC -shared -lOpenCL agg_gpu.c agg_cpu.c
 
