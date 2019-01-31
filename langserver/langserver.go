289cec67feeaba2b1371fb1d07cabe90edebac58
Jonathan A. Sternberg <jonathan@influxdata.com>
2019-01-31 14:27:05 -0800

    fake completion item yay

--- langserver/langserver.go
+++ langserver/langserver.go
@@ -6,8 +6,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
+	"net/url"
 
+	"github.com/influxdata/flux/complete"
 	"github.com/sourcegraph/go-lsp"
 	"github.com/sourcegraph/jsonrpc2"
 	"go.uber.org/zap"
@@ -19,6 +22,8 @@ type Server struct {
 	listener net.Listener
 	closed   bool
 	shutdown bool
+
+	workspace string
 }
 
 func New(h Handler, l *zap.Logger) *Server {
@@ -83,7 +88,13 @@ func (s *Server) handle(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.
 		if err := s.reset(params); err != nil {
 			return nil, err
 		}
-		return lsp.InitializeResult{}, nil
+		return lsp.InitializeResult{
+			Capabilities: lsp.ServerCapabilities{
+				CompletionProvider: &lsp.CompletionOptions{
+					TriggerCharacters: []string{"."},
+				},
+			},
+		}, nil
 	case "shutdown":
 		s.shutdown = true
 		return nil, nil
@@ -93,7 +104,17 @@ func (s *Server) handle(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.
 			return nil, err
 		}
 		return nil, nil
+	case "textDocument/completion":
+		if req.Params == nil {
+			return nil, &jsonrpc2.Error{Code: jsonrpc2.CodeInvalidParams}
+		}
+		var params lsp.CompletionParams
+		if err := json.Unmarshal(*req.Params, &params); err != nil {
+			return nil, err
+		}
+		return s.completions(params.TextDocument.URI)
 	}
+
 	return nil, &jsonrpc2.Error{
 		Code:    jsonrpc2.CodeMethodNotFound,
 		Message: fmt.Sprintf("method not supported: %s", req.Method),
@@ -101,5 +122,53 @@ func (s *Server) handle(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.
 }
 
 func (s *Server) reset(params lsp.InitializeParams) error {
+	if params.RootURI != "" {
+		u, err := url.Parse(string(params.RootURI))
+		if err != nil {
+			return err
+		}
+
+		if u.Scheme != "file" {
+			return fmt.Errorf("invalid uri scheme: %s", u.Scheme)
+		}
+		s.workspace = u.Path
+	}
 	return nil
+}
+
+func (s *Server) completions(uri lsp.DocumentURI) (lsp.CompletionList, error) {
+	text, err := s.getText(uri)
+	if err != nil {
+		return lsp.CompletionList{}, err
+	}
+	list, err := complete.StaticComplete(text)
+	if err != nil {
+		return lsp.CompletionList{}, err
+	}
+	items := make([]lsp.CompletionItem, 0, len(list))
+	for _, item := range list {
+		items = append(items, lsp.CompletionItem{
+			Label: item,
+		})
+	}
+	return lsp.CompletionList{
+		Items: items,
+	}, nil
+}
+
+func (s *Server) getText(uri lsp.DocumentURI) (string, error) {
+	u, err := url.Parse(string(uri))
+	if err != nil {
+		return "", err
+	}
+
+	if u.Scheme != "file" {
+		return "", fmt.Errorf("invalid uri scheme: %s", u.Scheme)
+	}
+
+	data, err := ioutil.ReadFile(u.Path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
 }
