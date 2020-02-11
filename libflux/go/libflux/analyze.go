051268eb53ade65afd735e1652b58ff781a94c43
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-02-11 16:16:59 -0600

    refactor: use pkg-config for compilation instead of flux-config

--- libflux/go/libflux/analyze.go
+++ libflux/go/libflux/analyze.go
@@ -2,9 +2,8 @@
 
 package libflux
 
-// #cgo CFLAGS: -I.
-// #cgo LDFLAGS: -L. -llibstd
-// #include "flux.h"
+// #cgo pkg-config: flux
+// #include "influxdata/flux.h"
 // #include <stdlib.h>
 import "C"
 
