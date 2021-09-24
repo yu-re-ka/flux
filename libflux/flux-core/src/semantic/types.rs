cea0ddcaf8a6ec6cff612e1377f30c461b890452
Markus Westerlind <mwesterlind@influxdata.com>
2021-09-24 12:44:27 +0200

    refactor: Add apply_ref to avoid the need for moving in apply

--- libflux/flux-core/src/semantic/types.rs
+++ libflux/flux-core/src/semantic/types.rs
@@ -88,6 +88,13 @@ impl Substitutable for PolyType {
             expr: self.expr.apply(sub),
         }
     }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        PolyType {
+            vars: self.vars.clone(),
+            cons: self.cons.clone(),
+            expr: self.expr.apply_ref(sub),
+        }
+    }
     fn free_vars(&self) -> Vec<Tvar> {
         minus(&self.vars, self.expr.free_vars())
     }
@@ -323,6 +330,24 @@ impl Substitutable for MonoType {
             MonoType::Dict(dict) => MonoType::Dict(Ptr::new(dict.apply(sub))),
             MonoType::Record(obj) => MonoType::Record(Ptr::new(obj.apply(sub))),
             MonoType::Fun(fun) => MonoType::Fun(Ptr::new(fun.apply(sub))),
+        }
+    }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        match self {
+            MonoType::Bool
+            | MonoType::Int
+            | MonoType::Uint
+            | MonoType::Float
+            | MonoType::String
+            | MonoType::Duration
+            | MonoType::Time
+            | MonoType::Regexp
+            | MonoType::Bytes => self.clone(),
+            MonoType::Var(tvr) => sub.apply(*tvr),
+            MonoType::Arr(arr) => MonoType::Arr(Ptr::new(arr.apply_ref(sub))),
+            MonoType::Dict(dict) => MonoType::Dict(Ptr::new(dict.apply_ref(sub))),
+            MonoType::Record(obj) => MonoType::Record(Ptr::new(obj.apply_ref(sub))),
+            MonoType::Fun(fun) => MonoType::Fun(Ptr::new(fun.apply_ref(sub))),
         }
     }
     fn free_vars(&self) -> Vec<Tvar> {
@@ -690,6 +715,9 @@ impl Substitutable for Array {
     fn apply(self, sub: &Substitution) -> Self {
         Array(self.0.apply(sub))
     }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        Array(self.0.apply_ref(sub))
+    }
     fn free_vars(&self) -> Vec<Tvar> {
         self.0.free_vars()
     }
@@ -742,6 +770,12 @@ impl Substitutable for Dictionary {
         Dictionary {
             key: self.key.apply(sub),
             val: self.val.apply(sub),
+        }
+    }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        Dictionary {
+            key: self.key.apply_ref(sub),
+            val: self.val.apply_ref(sub),
         }
     }
     fn free_vars(&self) -> Vec<Tvar> {
@@ -870,6 +904,15 @@ impl Substitutable for Record {
             Record::Extension { head, tail } => Record::Extension {
                 head: head.apply(sub),
                 tail: tail.apply(sub),
+            },
+        }
+    }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        match self {
+            Record::Empty => Record::Empty,
+            Record::Extension { head, tail } => Record::Extension {
+                head: head.apply_ref(sub),
+                tail: tail.apply_ref(sub),
             },
         }
     }
@@ -1088,6 +1131,12 @@ impl Substitutable for Property {
             v: self.v.apply(sub),
         }
     }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        Property {
+            k: self.k.clone(),
+            v: self.v.apply_ref(sub),
+        }
+    }
     fn free_vars(&self) -> Vec<Tvar> {
         self.v.free_vars()
     }
@@ -1184,6 +1233,10 @@ impl<T: Substitutable> Substitutable for Option<T> {
     fn apply(self, sub: &Substitution) -> Self {
         self.map(|t| t.apply(sub))
     }
+    // TODO
+    // fn apply_ref(&self, sub: &Substitution) -> Self {
+    //     self.as_ref().map(|t| t.apply_ref(sub))
+    // }
     fn free_vars(&self) -> Vec<Tvar> {
         match self {
             Some(t) => t.free_vars(),
@@ -1199,6 +1252,14 @@ impl Substitutable for Function {
             opt: self.opt.apply(sub),
             pipe: self.pipe.apply(sub),
             retn: self.retn.apply(sub),
+        }
+    }
+    fn apply_ref(&self, sub: &Substitution) -> Self {
+        Function {
+            req: self.req.apply_ref(sub),
+            opt: self.opt.apply_ref(sub),
+            pipe: self.pipe.apply_ref(sub),
+            retn: self.retn.apply_ref(sub),
         }
     }
     fn free_vars(&self) -> Vec<Tvar> {
